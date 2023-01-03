e212e1f7957b5eeb94601904ecdb4dbe1399f7a3
lilyshowlader <lilyshowlader@gmail.com>
2023-01-03 12:47:36 -0500

    add date picker

--- src/Components/Mife/Mife.jsx
+++ src/Components/Mife/Mife.jsx
@@ -1,9 +1,21 @@
+
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 function Mife () {
   // need a date picker here 
+  const [startDate, setStartDate] = useState(new Date());
   return (
   <>
   <p>what time do you plan on taking mifepristone?</p>
   <p>(the first pill)</p>
+  <DatePicker 
+  selected={startDate}
+  onChange={(date) => setStartDate(date)}
+  showTimeSelect
+  dateFormat="Pp" />
+  
   </>
   )
 }
