63fe816f58a71fc91f4d7e60f30139c74f6f2ab9
lilyshowlader <lilyshowlader@gmail.com>
2023-01-03 12:22:16 -0500

    add landing component

---
+++ src/Components/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+function Landing () {
+  return (
+    <>
+    <p>hey there</p>
+    <p>today might be hard</p>
+    <p>but we are here for you</p>
+    <button>click here to continue </button>
+    </>
+  )
+}
+
+export default Landing
