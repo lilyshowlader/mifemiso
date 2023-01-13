cf0478a26faafef44ace8ef03bd144633da98543
lilyshowlader <lilyshowlader@gmail.com>
2023-01-12 19:48:46 -0500

    add twentyfour hour time stamp

--- src/Components/Mife/Mife.jsx
+++ src/Components/Mife/Mife.jsx
@@ -5,19 +5,46 @@ import "react-datepicker/dist/react-datepicker.css";
 
 function Mife () {
   // need a date picker here 
-  const [startDate, setStartDate] = useState(new Date());
+  // const [startDate, setStartDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
+  // const [startRange, setStartRange] = useState('')
+  // const [endRange, setEndRange] = useState('')
+
+  
+// function addEightHours(date, hours) {
+//   date.setHours(date.getHours() + hours);
+
+//   return date;
+// }
+// const start = addEightHours(date, 8);
+// console.log(start);
+
+function addTwentyFourHours(date, hours) {
+  date.setHours(date.getHours() + hours);
+
+  return date;
+}
+const end = addTwentyFourHours(date, 24);
+console.log(end);
+
+
   return (
   <>
   <p>what time do you plan on taking mifepristone?</p>
   <p>(the first pill)</p>
   <DatePicker 
-  selected={startDate}
-  onChange={(date) => setStartDate(date)}
+  selected={date}
+  onChange={(date) => setDate(date)}
   showTimeSelect
-  dateFormat="Pp" />
-  
+  dateFormat="Pp"/>
+  <p>based on your input, you may take the misoprostol between:</p>
+  {/* <p>{startDate}</p> */}
+  <div>Selected start date={date ? date.toString() : null}</div>
+  {/* <p>start:{start}</p> */}
+  {/* <p>end: {end}</p> */}
+
   </>
   )
 }
-
+ 
 export default Mife
