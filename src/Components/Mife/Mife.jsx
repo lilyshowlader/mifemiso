30724c88903c93458d09c8f4ec5c777090b62749
lilyshowlader <lilyshowlader@gmail.com>
2023-01-18 20:42:00 -0500

    remove handleDateChange from return, modify newDate attributes

--- src/Components/Mife/Mife.jsx
+++ src/Components/Mife/Mife.jsx
@@ -2,52 +2,86 @@ import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Link } from "react-router-dom";
-function Mife () {
-  const initialDate = new Date()
-  const [twentyFourHours, setTwentyFourHours] = useState(new Date());
-  const [eightHours, setEightHours] = useState(new Date())
 
+function Mife() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
 
-  // function below to add eight hours to initialDate
-  function addEightHours(initialDate, hours) {
-    eightHours.setHours(eightHours.getHours() + hours);
-    return eightHours;
+  // function to add 8 hours to the selectedDate
+  function calculateStartDate(date) {
+    const newDate = new Date(date);
+    newDate.setFullYear(date.getFullYear());
+    newDate.setMonth(date.getMonth());
+    newDate.setDate(date.getDate());
+    newDate.setHours(date.getHours() + 8);
+    newDate.setMinutes(date.getMinutes());
+    return newDate;
   }
-  const start = addEightHours(eightHours, 8);
 
-  // function below to add twenty four hours to initialDate
-  function addTwentyFourHours(initialDate, hours) {
-    twentyFourHours.setHours(twentyFourHours.getHours() + hours)
-    return twentyFourHours;
+  // function to add 24 hours to the selectedDate
+  function calculateEndDate(date) {
+    const newDate = new Date(date);
+    newDate.setFullYear(date.getFullYear());
+    newDate.setMonth(date.getMonth());
+    newDate.setDate(date.getDate());
+    newDate.setHours(date.getHours() + 24);
+    newDate.setMinutes(date.getMinutes());
+    return newDate;
   }
-  const end = addTwentyFourHours(twentyFourHours, 24);
+
+  // onChange event handler for the datepicker
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    setStartDate(calculateStartDate(date));
+    setEndDate(calculateEndDate(date));
+  };
 
   return (
-  <>
-  <p>what time do you plan on taking mifepristone?</p>
-  <p>(the first pill)</p>
-  <DatePicker 
-  selected={initialDate}
-  onChange={(initialDate) =>{setEightHours(initialDate); setTwentyFourHours(initialDate)}}
-  showTimeSelect
-  dateFormat="Pp"/>
-  <p>based on your input, you may take the misoprostol between:</p>
-  <p>start:{start.toString()}</p>
-  <p> and </p>
-  <p>end: {end.toString()}</p>
-  <Link to="/instructions">
-      <button type="button">
-      click here to continue
-      </button>
-  </Link>
-  <Link to="/pilldescription">
-      <button type="button">
-      back
-      </button>
-  </Link>
-  </>
-  )
-}
- 
-export default Mife                     
+    <>
+      <p>what time do you plan on taking mifepristone?</p>
+      <p>(the first pill)</p>
+      <DatePicker
+        selected={selectedDate}
+        onChange={handleDateChange}
+        showTimeSelect
+        dateFormat="Pp"
+      />
+      <p>based on your input, you may take the misoprostol between:</p>
 
+      <p> start:{" "}
+        {startDate ? startDate.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      }) : "please select a date"}
+      </p>
+
+      <p> and </p>
+
+      <p>
+        end:{" "}
+        {endDate ? endDate.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+        }) : "please select a date"}
+      </p>
+      <Link to="/instructions">
+        <button type="button">click here to continue</button>
+      </Link>
+      <Link to="/pilldescription">
+        <button type="button">back</button> 
+        </Link>
+
+        </>
+  );
+}
+
+export default Mife;
