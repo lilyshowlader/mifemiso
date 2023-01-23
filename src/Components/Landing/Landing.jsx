855af342b037f4b8443340b9e1df6f27b468f87a
lilyshowlader <lilyshowlader@gmail.com>
2023-01-23 15:50:21 -0500

    edit landing button

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
+import 'animate.css';
 
 function Landing () {
   return (
     <>
     <div className='landing-container'>
     <div className='logo'>mifemiso</div>
-    <div className='logo-sub'>we'll walk you through.</div>
+    <div className="animate__animated animate__slideInDown">we'll walk you through.</div>
     <Link to="/pilldescription">
-      <button type="button">
+      <button type="button" className='landingbutton'>
       click 
       </button>
     </Link>
