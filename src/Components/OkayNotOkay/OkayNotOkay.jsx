cd867db07ece380c3944a147b80fbc5aa0dca07f
lilyshowlader <lilyshowlader@gmail.com>
2023-01-23 15:18:49 -0500

    add medication pictures

--- src/Components/OkayNotOkay/OkayNotOkay.jsx
+++ src/Components/OkayNotOkay/OkayNotOkay.jsx
@@ -3,12 +3,35 @@ import { Link } from "react-router-dom"
 function OkayNotOkay () {
   return (
     <>
-    <p>what's normal?</p>
+    <div className='expectations-container'>
+    <p className='expectations-title'>what's normal?</p>
+    <p className='description'>cramping and bleeding usually begin 1-4 hours after taking misoprostol. sometimes it can even take up to 24 hours. usualy cramping begins before bleeding. please use the heating pad that was given to you, this can help with the cramping. </p>
+    <p className='description'>it's common to feel all kinds of emotions as you go through this process. allow yourself to feel the emotions. the hormones are powerful! it's totally okay to feel the full range of emotions and feel that they are conflicting with eachother. anxiety is common and it is normal to feel anxiety when you do not know what to expect. please call our helpline if you are concerned and need some reassurance.</p>
+
+    <p className='expectations-title'>when to call the doctor</p>
+    <p className='description'>if you do not bleed within 24 hours, please call the doctor. the doctor will likely instruct you to repeat a 2nd round of misoprostol, 24 hours after the first dose</p>
+    <ul className='description'>other reasons to call the doctor:
+    <li className='description'>fever of 100.4 or higher</li>
+    <li className='description'>sudden severe one-sided (left or right) lower abdominal pain</li>
+    <li className='description'>heavy bleeding (soaking through two thick sanitary pads per hour for two hours in a row)</li>
+    <li className='description'>feeling weakness, nausea, vomitting, diarrhea > 24 hours after misoprostol </li>
+    </ul>
+
+    <p className='expectations-title'>follow up</p>
+    <ul>
+    <li className='description'>use sanitary pads, not tampons, for 48 hours, then change tampons frequently</li>
+    <li className='description'>if you plan on having sex, please use condoms for 7 days</li>
+    <li className='description'>call the doctor if you do not bleed within 24 hours of taking misoprostol.</li>
+    <li className='description'>you must go to your follow-up appointment that was scheduled for you. during this appointment, the doctor will do a follow-up ultrasound to make sure everything went okay!</li>
+    </ul>
+
+    
     <Link to="/instructions">
       <button type="button">
       back
       </button>
     </Link>
+    </div>
     </>
   )
 }
--- src/Components/Pill Description/PillDescription.jsx
+++ src/Components/Pill Description/PillDescription.jsx
@@ -5,16 +5,20 @@ function PillDescription () {
     <div className='pilldescription-container'>
   
     <p className='pill-title'>here is a list of all the medications<br></br> that were given to you.</p> 
+    <img src="https://i.imgur.com/TOSVkTZ.png" alt='mifepristone'></img>
     <p className='medication-names'>mifepristone</p>
     <p className='medication-description'>mifepristone is the first pill you will take during this process. mifepristone works by stopping the supply of hormones that maintains the interior of the uterus. without these hormones, the uterus cannot support the pregnancy and the contents of the uterus are expelled.</p>
+    <img src="https://i.imgur.com/evBYUOl.png" alt='misoprostol'></img>
     <p className='medication-names'>misoprostol</p>
-    <p className='medication-description'>misoprostol is what is used to help "empty" your uterus. we recommend you take misoprostol either vaginally or buccally. we're gonna explain this, don't worry.</p>
+    <p className='medication-description'>you were given 4 misoprostol pills. misoprostol is what is used to help "empty" your uterus. we recommend you take misoprostol either vaginally or buccally. we're gonna explain this, don't worry.</p>
+    <img src="https://i.imgur.com/qLtg1ns.png" alt='compazine'></img>
     <p className='medication-names'>compazine</p>
-    <p className='medication-description'>compazine is used to treat nausea.</p>
+    <p className='medication-description'>compazine is used to treat nausea. we recommend for patients to take this medication as a preventative measure.</p>
+    <img src="https://i.imgur.com/h83dewV.png" alt='ibuprofen'></img>
     <p className='medication-names'>ibuprofen</p>
-    <p className='medication-description'>ibuprofen is used for pain management.</p>
+    <p className='medication-description'>ibuprofen is used for pain management.we recommend for patients to take this medication as a preventative measure. </p>
     <p className='medication-names'>percocet</p>
-    <p className='medication-description'>percocet is also used for pain management.</p>
+    <p className='medication-description'>percocet is a controlled substance that is also used for pain management. we have given you a perscription for this medication. please pick it up from your pharmacy. we recommend for patients to take this medication as a preventative measure.</p>
     <Link to="/mife">
       <button type="button">
       continue
