6ae81bb61083831e4c228d2bd34b03ea6f0fd154
lilyshowlader <lilyshowlader@gmail.com>
2023-01-23 19:59:47 -0500

    edit mobile disclaimer page

--- src/Components/Disclaimer/Disclaimer.jsx
+++ src/Components/Disclaimer/Disclaimer.jsx
@@ -6,7 +6,7 @@ function Disclaimer () {
   <>
   <div className='landing-container'>
     <div className='logo'>mifemiso</div>
-    <p className='disclaimer'>disclaimer: this app was created as a personal project to demonstrate the concept of a women's health app that simplifies the abortion medication process. this app does not replace consultation with a doctor.</p>
+    <p className='disclaimer'>disclaimer: <br></br>this app was created as a personal project to demonstrate the concept of a women's health app that simplifies the abortion medication process. this app does not replace consultation with a doctor.</p>
     <Link to="/mifemiso">
       <button type="button" className='landingbutton'>
       I understand
