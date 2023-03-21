72bd7e1d96f59999d86362b2f9223dfa45d433b6
Ibrahim Saberi <georgeipsum@gmail.com>
2023-03-21 11:06:19 -0400

    create context object for beginning mifepristone date

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import Landing from './Components/Landing/Landing';
 import PillDescription from './Components/Pill Description/PillDescription';
@@ -10,20 +11,32 @@ import Disclaimer from './Components/Disclaimer/Disclaimer';
 import MisoOptions from './Components/MisoOptions/MisoOptions';
 import MifeBuccal from './Components/Mife/MifeBuccal';
 import InstructionsBuccal from './Components/Instructions/InstructionsBuccal'
+import { MifoDateProvider } from './utils/useMifoDate';
 
 function App() {
+  const [date, setDate] = useState((() => {
+    const date = new Date();
+    date.setMilliseconds(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setHours(0);
+    return date;
+  })());
+
   return (
-    <Routes>
-      <Route path='/' element={<Disclaimer />} />
-      <Route path='/mifemiso' element={<Landing />} />
-      <Route path='/pilldescription' element={<PillDescription />} />
-      <Route path='/misooptions' element={<MisoOptions />} />
-      <Route path='/mifeMisoVaginal' element={<Mife />} />
-      <Route path='/mifeMisoBuccal' element={<MifeBuccal />} />
-      <Route path='/instructions' element={<Instructions />} />
-      <Route path='/buccalinstructions' element={<InstructionsBuccal />} />
-      <Route path='/okaynotokay' element={<OkayNotOkay />} />
-    </Routes>
+    <MifoDateProvider value={{ date, setDate }}>
+      <Routes>
+        <Route path='/' element={<Disclaimer />} />
+        <Route path='/mifemiso' element={<Landing />} />
+        <Route path='/pilldescription' element={<PillDescription />} />
+        <Route path='/misooptions' element={<MisoOptions />} />
+        <Route path='/mifeMisoVaginal' element={<Mife />} />
+        <Route path='/mifeMisoBuccal' element={<MifeBuccal />} />
+        <Route path='/instructions' element={<Instructions />} />
+        <Route path='/buccalinstructions' element={<InstructionsBuccal />} />
+        <Route path='/okaynotokay' element={<OkayNotOkay />} />
+      </Routes>
+    </MifoDateProvider>
   );
 }
 
