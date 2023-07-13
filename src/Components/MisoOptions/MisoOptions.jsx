7f6c9a0aee042747906deabac1b83e79cb21cc10
lilyshowlader <lilyshowlader@gmail.com>
2023-07-13 09:35:52 -0400

    fix capitalization

--- src/Components/MisoOptions/MisoOptions.jsx
+++ src/Components/MisoOptions/MisoOptions.jsx
@@ -5,7 +5,7 @@ function MisoOptions() {
     <>
       <div className='container miso-options-container'>
         <p className='options-title'>which one works for you?</p>
-        <p>The second part of this process includes inserting four pills either vaginally (in the vagina) or buccally (pills are placed in between your cheeks and teeth). Which option do you feel more comfortable with?</p>
+        <p>the second part of this process includes inserting four pills either vaginally (in the vagina) or buccally (pills are placed in between your cheeks and teeth). Which option do you feel more comfortable with?</p>
         <div className='buttons-container'>
           <div className='option'>
           <img src="https://i.imgur.com/pKR9Nyh.jpg" alt='vaginal' className='fruitpic' />
