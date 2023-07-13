700bbd09ce8572225e0e3f855e0f109760888e5e
lilyshowlader <lilyshowlader@gmail.com>
2023-07-13 09:29:01 -0400

    add images

--- src/Components/MisoOptions/MisoOptions.jsx
+++ src/Components/MisoOptions/MisoOptions.jsx
@@ -1,20 +1,26 @@
 import { Link } from "react-router-dom";
 
-function MisoOptions () {
+function MisoOptions() {
   return (
     <>
-    <div className='container miso-options-container'>
-    <p className='options-title'>which one works for you?</p>
-    <p> the second part of this process includes inserting four pills either vaginally (in the vagina) or buccally (pills are placed in between your cheeks and teeth). which option do you feel more comfortable with?</p>
-    <div className='buttons-container'>
-    <Link to="/mifeMisoVaginal">
-        <button type="button">vaginal</button>
-    </Link>
-    <Link to="/mifeMisoBuccal">
-        <button type="button">buccal</button>
-    </Link>
-    </div>
-    </div>
+      <div className='container miso-options-container'>
+        <p className='options-title'>which one works for you?</p>
+        <p>The second part of this process includes inserting four pills either vaginally (in the vagina) or buccally (pills are placed in between your cheeks and teeth). Which option do you feel more comfortable with?</p>
+        <div className='buttons-container'>
+          <div className='option'>
+          <img src="https://i.imgur.com/pKR9Nyh.jpg" alt='vaginal' className='fruitpic' />
+            <Link to="/mifeMisoVaginal">
+              <button type="button">vaginal</button>
+            </Link>
+          </div>
+          <div className='option'>
+          <img src="https://i.imgur.com/JYWzYjY.jpg" alt='buccal' className='fruitpic' />
+            <Link to="/mifeMisoBuccal">
+              <button type="button">buccal</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
